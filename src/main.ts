3000f192fb62a024f86cc8a025656cb2b36b33a9
LoneRifle <LoneRifle@users.noreply.github.com>
2024-01-30 17:12:51 +0800

    build(vercel): rejig to accommodate build norms
    - Shard `app` from `main`, to allow others to obtain a Nest instance
      - Also add export of response handler wrapper for vercel
    - Add `api/index.js` shim for vercel, `public/robots.txt` to trick vercel
    - Rewrite Vercel config to make use of shim

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,7 @@
-import 'dotenv/config'
-import { NestFactory } from '@nestjs/core'
-import { getBotToken } from 'nestjs-telegraf'
-import cookieParser from 'cookie-parser'
-
-import { AppModule } from './app.module'
-import { ConfigService } from '@nestjs/config'
-import { DatabaseService } from './database/database.service'
-import { session } from 'telegraf'
-import { executeMigration } from './database/utils/pg.utils'
+import { appPromise } from './app'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  const configService = app.get<ConfigService>(ConfigService)
-  const databaseService = app.get<DatabaseService>(DatabaseService)
-
-  app.use(cookieParser())
-
-  const bot = app.get(getBotToken())
-  bot.use(
-    session({
-      store: databaseService.store,
-      getSessionKey: (ctx) => `${ctx.from.id}`,
-    }),
-  )
-  app.use(bot.webhookCallback(configService.get<string>('bot.path')))
-  await app.listen(3000)
+  const app = await appPromise
+  app.listen(3000)
 }
 bootstrap()
